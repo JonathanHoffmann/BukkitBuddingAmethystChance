030b9adc1f46927049b6b5a135a6de3f002e8441
Jonathan Hoffmann <jonathan.hoffmann@gmx.net>
2025-02-26 07:38:50 +0100

    Update BuddingAmethystChance.java

--- src/main/java/me/JonathanHoffmann/BuddingAmethystChance/BuddingAmethystChance.java
+++ src/main/java/me/JonathanHoffmann/BuddingAmethystChance/BuddingAmethystChance.java
@@ -1,19 +1,10 @@
 package me.JonathanHoffmann.BuddingAmethystChance;
 
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
-import java.io.IOException;
 
 public class BuddingAmethystChance extends JavaPlugin {
     String configpath = "chance%";
+
     @Override
     public void onEnable() {
         loadConfig();
